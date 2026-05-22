27f07f0f66d34e4a846bf90241ba2bac9a98fbe8
MaiAsai <mai100uta@gmail.com>
2026-05-22 18:35:10 +0900

    レシピ編集/詳細 文字色修正

--- src/app/(main)/recipes/styles/PageTitle.tsx
+++ src/app/(main)/recipes/styles/PageTitle.tsx
@@ -1,20 +1,15 @@
 //ページタイトル　スタイル管理用コンポーネント
 
-'use client'
+'use client';
 
-type PageTitleProps = { 
-  children : React.ReactNode
-}
+type PageTitleProps = {
+  children: React.ReactNode;
+};
 
-const PageTitle = ({ children }:PageTitleProps) => {
-
-  return(
-    <h1
-      className="w-full border-b text-xl text-center text-gray-400 my-4"
-    >
-      { children }
-    </h1>
-  )
-}
+const PageTitle = ({ children }: PageTitleProps) => {
+  return (
+    <h1 className="w-full border-b text-xl text-center my-4">{children}</h1>
+  );
+};
 
 export default PageTitle;
